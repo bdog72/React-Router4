f059d86a036a6896ef5dab0787da043f48087b42
Brian Bycynski <high5bri@hotmail.com>
2017-06-19 05:51:15 -0400

    made home routing page

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,49 @@
 import React, { Component } from 'react'
 
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
 class App extends Component {
   render () {
     return <div>
-      <h1>Hello, World!</h1>
+      <BasicExample />
     </div>
   }
 }
 
 export default App
+
+const BasicExample = () => (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to='/'>Example1</Link></li>
+        <li><Link to='/ex2'>Example2</Link></li>
+        <li><Link to='/ex3'>Example3</Link></li>
+      </ul>
+
+      <hr />
+
+      <Route exact path='/' component={Ex1} />
+      <Route path='/ex2' component={Ex2} />
+      <Route path='/ex3' component={Ex3} />
+    </div>
+  </Router>
+)
+
+const Ex1 = () => (
+  <div>
+    <h2>example 1</h2>
+  </div>
+)
+
+const Ex2 = () => (
+  <div>
+    <h2>example 2</h2>
+  </div>
+)
+
+const Ex3 = () => (
+  <div>
+    <h2>example 3</h2>
+  </div>
+)
