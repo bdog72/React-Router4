8b39532f0b1f77579e429310144c654347b11ad9
Brian Bycynski <high5bri@hotmail.com>
2017-06-21 07:09:15 -0400

    added another example

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import BasicExample1 from './Example1'
 import ParamsExample from './Example2'
+import AuthExample from './Example3'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 class App extends Component {
@@ -20,6 +21,7 @@ const BasicExample = () => (
         <li><Link to='/'>HOME</Link></li>
         <li><Link to='/ex1'>Example1</Link></li>
         <li><Link to='/ex2'>Example2</Link></li>
+        <li><Link to='/ex3'>Example3</Link></li>
       </ul>
 
       <hr />
@@ -27,6 +29,7 @@ const BasicExample = () => (
       <Route exact path='/' component={Home} />
       <Route path='/ex1' component={Ex1} />
       <Route path='/ex2' component={Ex2} />
+      <Route path='/ex3' component={Ex3} />
     </div>
   </Router>
 )
@@ -46,5 +49,11 @@ const Ex1 = () => (
 const Ex2 = () => (
   <div>
     <ParamsExample />
+  </div>
+)
+
+const Ex3 = () => (
+  <div>
+    <AuthExample />
   </div>
 )
